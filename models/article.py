5e00ca2ee7ffffdb4551d0f335a67e8833177b91
Kateryna Kandziuba <katerynakandziuba@gmail.com>
2021-09-24 18:40:57 +0300

    added news displaying

--- models/article.py
+++ models/article.py
@@ -1,7 +1,31 @@
-from config import MySQL
+from config import mysql
+
 
 class Article(object):
 
     @staticmethod
+    def add_article(title, about, content, image, publish, user_id, status):
+        connection = mysql.get_db()
+        cursor = connection.cursor()  # создаем курсор - итератор внутри базы данных
+        sql_query = """
+            insert into news (title, about, content, image, publish, user_id, status)
+            values (%s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(sql_query, (title, about, content, image, publish, user_id, status))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    @staticmethod
     def get_all_articles() -> list:
-        pass
+        connection = mysql.get_db()
+        cursor = connection.cursor()  # создаем курсор - итератор внутри базы данных
+        sql_query = """
+            select * from news
+        """
+        cursor.execute(sql_query)
+        # ничего не коммитим потому что в базе ничего не изменили
+        result = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return result
